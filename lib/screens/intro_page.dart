88e3af6e5684a3bb6b26937176342e01630379f7
Phsithzz <phoosith2548@gmail.com>
2025-11-25 21:22:34 +0700

    ThemeData

---
+++ lib/screens/intro_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body:Column(
+        children: [
+          //logo
+        
+          //title
+
+          //subtitle
+        ],
+      )
+      );
+  }
+}
