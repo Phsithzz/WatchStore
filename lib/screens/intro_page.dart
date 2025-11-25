bd7fd315fbfdef8d064b7fd79a0cbe5382dd2f83
Phsithzz <phoosith2548@gmail.com>
2025-11-25 21:36:47 +0700

    introPage

--- lib/screens/intro_page.dart
+++ lib/screens/intro_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ecom/components/button.dart';
 
 class IntroPage extends StatelessWidget {
   const IntroPage({super.key});
@@ -7,15 +8,36 @@ class IntroPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.surface,
-      body:Column(
-        children: [
-          //logo
-        
-          //title
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            //logo
+            Icon(
+              Icons.shopping_bag,
+              size: 72,
+              color: Theme.of(context).colorScheme.inversePrimary,
+            ),
+            const SizedBox(height: 10),
+            //title
+            const Text(
+              "Watch Store",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            ),
 
-          //subtitle
-        ],
-      )
-      );
+            //subtitle
+            Text(
+              "Primium Quality Products",
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.inversePrimary,
+              ),
+            ),
+            const SizedBox(height: 10),
+            //button
+            MyButton(onTap: () {}, child: Icon(Icons.arrow_forward)),
+          ],
+        ),
+      ),
+    );
   }
 }
